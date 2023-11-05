db8c2395fbb53eb6135123088d101383b6e262f6
Sahib Bhai <lunarwolf01@gmail.com>
2023-11-04 17:54:03 -0700

    Refactor transaction amount formatting for readability
    - Updated the format_amount function to handle negative values correctly, ensuring the negative sign precedes the dollar sign, aligning with standard financial representations.
    - Included thousands separator and two decimal places for better clarity and readability in financial figures.
    - Adjusted the dataframe creation to include the newly formatted 'amount' along with 'date', 'memo', and 'category_name' for complete transaction details.

--- ynab.py
+++ ynab.py
@@ -1,8 +1,8 @@
-from dotenv import load_dotenv
-from typing import List, Dict, Any
-import os
-import requests
 import pandas as pd
+import requests
+from typing import List, Dict, Any
+from dotenv import load_dotenv
+import os
 
 # Load environment variables from .env file
 load_dotenv()
@@ -11,17 +11,32 @@ load_dotenv()
 access_token = os.environ.get("YNAB_ACCESS_TOKEN")
 budget_id = os.environ.get("YNAB_BUDGET_ID")
 
+def format_amount(milliunits: int) -> str:
+    """
+    Converts an amount in milliunits to a formatted string in dollars.
+
+    :param milliunits: The amount in milliunits.
+    :return: A formatted string in dollars with thousands separator and two decimal places.
+    """
+    dollars = milliunits / 1000
+    formatted_amount = f"${abs(dollars):,.2f}"  # Format the positive amount in dollars with thousands separator and two decimal places.
+    
+    if milliunits < 0:
+        return f"-{formatted_amount}"  # Prepend the negative sign for negative amounts.
+    else:
+        return formatted_amount
+
 def get_ynab_transactions(
     access_token: str, budget_id: str, memo_filter: str
 ) -> pd.DataFrame:
     """
     Fetches a list of transactions from the YNAB API for a specified budget and filters them by memo.
-    Returns a pandas DataFrame containing 'date', 'category_name', and 'memo' for each filtered transaction.
+    Returns a pandas DataFrame containing 'date', 'amount', 'category_name', and 'memo' for each filtered transaction.
 
     :param access_token: Your YNAB personal access token
     :param budget_id: The YNAB budget ID from which to retrieve transactions
     :param memo_filter: A substring to search for in each transaction's memo field (case insensitive)
-    :return: A pandas DataFrame containing 'date', 'category_name', and 'memo' columns.
+    :return: A pandas DataFrame containing 'date', 'amount', 'category_name', and 'memo' columns.
     """
     # Endpoint format for transactions in a specific budget
     url = f"https://api.youneedabudget.com/v1/budgets/{budget_id}/transactions"
@@ -39,10 +54,11 @@ def get_ynab_transactions(
         # Parse the response JSON and retrieve all transactions
         transactions = response.json()['data']['transactions']
         
-        # Filter the transactions
+        # Filter the transactions and prepare the DataFrame
         filtered_transactions = [
             {
                 'date': transaction['date'],
+                'amount': format_amount(transaction['amount']),
                 'category_name': transaction['category_name'],
                 'memo': transaction['memo']
             }
@@ -51,7 +67,7 @@ def get_ynab_transactions(
         ]
         
         # Create DataFrame
-        df = pd.DataFrame(filtered_transactions, columns=['date', 'category_name', 'memo'])
+        df = pd.DataFrame(filtered_transactions)
         return df
     else:
         # Handle possible errors
